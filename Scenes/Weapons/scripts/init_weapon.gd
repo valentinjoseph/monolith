f4686a04f4496f91fe199530cf84bd2f215ca2f9
Valentin Joseph <96952537+valentinjoseph@users.noreply.github.com>
2025-02-19 17:52:44 +0100

    Add files via upload

---
+++ Scenes/Weapons/scripts/init_weapon.gd
@@ -0,0 +1,89 @@
+@tool
+
+extends Node3D
+
+@export var weapon_type: Weapons:
+	set(value):
+		weapon_type = value
+		if Engine.is_editor_hint():
+			load_weapon()
+
+@export var sway_noise: NoiseTexture2D
+@export var sway_speed: float= 1.2
+@export var reset : bool=false:
+	set(value):
+		reset=value
+		if Engine.is_editor_hint():
+			load_weapon()
+			
+			
+@onready var weapon_mesh: MeshInstance3D = %WeaponMesh
+
+var mouse_movement:Vector2
+var random_sway_x
+var random_sway_y
+var random_sway_amount:float
+var time: float = 0.0
+var idle_sway_adjustment
+var idle_sway_rotation_strength
+
+
+func _ready()->void:
+	await owner.ready
+	load_weapon()
+
+func _input(event):
+	if event.is_action_pressed("weapon1"):
+		weapon_type=load("res://Models/weapons/crowbar/CrowbarResource.tres")
+		load_weapon()
+	#if event.is_action_pressed("weapon2"):
+		#weapon_type=load("res://Models/weapons/crowbar2/Crowbar2Resource.tres")
+		#load_weapon()
+		
+	if event is InputEventMouseMotion:
+		mouse_movement=event.relative	
+		
+func load_weapon()->void:
+	weapon_mesh.mesh=weapon_type.mesh #set weapon mesh
+	position=weapon_type.position #set weapon position
+	rotation_degrees = weapon_type.rotation #set weapon rotation
+	scale=weapon_type.scale #set weapon scale
+	idle_sway_adjustment=weapon_type.idle_sway_adjustment
+	idle_sway_rotation_strength=weapon_type.idle_sway_rotation_strength
+	random_sway_amount=weapon_type.random_sway_amount
+
+func sway_weapon(delta)-> void:
+	#if Engine.is_editor_hint():
+		#return
+		
+	#get random sway value from 2D noise
+	var sway_random:float=get_sway_noise()
+	var sway_random_adjusted: float= sway_random * idle_sway_adjustment #adjust sway strength
+	
+	#create time with delta and set two sine values for x and y sway movement
+	time += delta * (sway_speed + sway_random)
+	random_sway_x = sin(time * 1.5 + sway_random_adjusted)/ random_sway_amount
+	random_sway_y = sin(time - sway_random_adjusted)/ random_sway_amount
+	
+	#clamp mouse movement
+	mouse_movement= mouse_movement.clamp(weapon_type.sway_min, weapon_type.sway_max)
+	#lerp weapon position based on mouse movement
+	position.x = lerp(position.x, weapon_type.position.x - (mouse_movement.x * weapon_type.sway_amount_position + random_sway_x) * delta, weapon_type.sway_speed_position)
+	position.y = lerp(position.y, weapon_type.position.y + (mouse_movement.y * weapon_type.sway_amount_position + random_sway_y) * delta, weapon_type.sway_speed_position)
+	#lerp weapon rotation based on mouse movement
+	rotation_degrees.y = lerp(rotation_degrees.y, weapon_type.rotation.y + (mouse_movement.y * weapon_type.sway_amount_rotation + (random_sway_y * idle_sway_rotation_strength)) * delta, weapon_type.sway_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, weapon_type.rotation.x - (mouse_movement.x * weapon_type.sway_amount_rotation + (random_sway_x * idle_sway_rotation_strength)) * delta, weapon_type.sway_speed_rotation)
+	
+	
+func get_sway_noise()->float:
+	var player_position: Vector3=Vector3(0,0,0)
+	#only access global variable when in game to avoid constant errors
+	if not Engine.is_editor_hint():
+		player_position=Global.player.global_position
+		
+	var noise_location: float = sway_noise.noise.get_noise_2d(player_position.x, player_position.y)
+	return noise_location
+	
+	
+func _physics_process(delta: float)->void:
+	sway_weapon(delta)
