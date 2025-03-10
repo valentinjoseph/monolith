ef4868e6d6a1fe9deaccba9340ff4bc1021ffa8d
Valentin Joseph <valino_joseph@hotmail.com>
2025-03-10 21:48:07 +0100

    UPD1003

---
+++ UI/global 2.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+var debug #references the DebugPanel
+var player: Player #references to the player 
+var ui_context : ContextComponent #reference to Interaction UI
+var enemy: EnemyController #references to the enemy
